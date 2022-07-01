f4e0841b2c5da395a985f2536d096639df56e466
Pranay <pranay2811@gmail.com>
2022-07-01 11:21:31 -0400

    Dependency on rrapply

--- R/Basic.R
+++ R/Basic.R
@@ -8,8 +8,8 @@
 #' @importFrom rstudioapi askForSecret
 #' @importFrom jsonlite toJSON
 #' @importFrom jsonlite fromJSON
-#' @importFrom rrapply rrapply
 #' @importFrom stringr str_split
+#' @importFrom rrapply rrapply
 
 # Set API keys and URL ----------------------------------------------------------
 
@@ -251,17 +251,17 @@ get_deployments <- function(org_id){
 
 ##Query All Equipment Types
 #' @export
-get_equip_types <- function(){
-
+get_equip_types_clean <- function(){
+  
   equiptype <-api.get('equiptype')
-
+  
   #Get subtypes
   subtype<-rrapply(equiptype$sub_types,how='melt') %>%
     mutate_at(vars(value),
               ~gsub('c\\(|\\(|\\"|\\)','',.))
-
+  
   char_split<-data.frame(str_split(subtype$value,', ',n=Inf,simplify=T))
-
+  
   subtype_format <- bind_cols(subtype,char_split) %>%
     select(-value) %>%
     filter(L2!='id')  %>%
@@ -270,45 +270,45 @@ get_equip_types <- function(){
     pivot_wider(id=c(L1,names),names_from = L2,values_from = 'values') %>%
     select(-c(L1,names)) %>%
     mutate_at(vars(equipment_type_id),~as.integer(.))
-
+  
   equip_types <- equiptype %>%
     filter(active==T) %>%
     select(-c(sub_types,critical_point_types,flow_order,active)) %>%
     left_join(subtype_format,by=c('id'='equipment_type_id'),
               suffix = c('','_subtype'))
-
+  
   return(equip_types)
 }
 
 ## Get Point types, measurements and their units in a clean output
 #' @export
 get_point_types <- function(){
-
+  
   pointtypes <- api.get('pointtypes')
-
+  
   units <- api.get('unit')
-
+  
   measurements <- api.get('measurements')
-
+  
   # Get measurements and their associated units
   measurement_unit_df <- data.frame()
-
+  
   for (i in 1:nrow(measurements)){
     row_num =i
-
+    
     measurement_single <- measurements[row_num,]
-
-
+    
+    
     unit_list <- measurement_single$units
-
+    
     if(!nrow(unit_list[[1]])==0){
-
-
+      
+      
       unit  <-  data.frame(t(sapply(unit_list,c))) %>%
         select(unit_name=name_long,unit=name_abbr,data_type) %>%
         mutate_all(~gsub('c\\("|"|\\)','',.)) %>%
         mutate(name=measurement_single$name)
-
+      
       measurement_unit_df_single <- left_join(measurement_single,unit,
                                               by=c('name')) %>%
         select(id,measurement_name=name,
@@ -317,11 +317,11 @@ get_point_types <- function(){
                unit_name,
                unit,
                data_type)
-
+      
       measurement_unit_df <- rbind(measurement_unit_df,measurement_unit_df_single)
     }
   }
-
+  
   #Unite data frames
   point_types <- left_join(select(pointtypes,id,tag_name,measurement_id,tags),
                            measurement_unit_df,
@@ -333,11 +333,10 @@ get_point_types <- function(){
            unit,
            data_type,
            tags)
-
+  
   return(point_types)
-
+  
 }
-
 
 # Point Selector ----------------------------------------------------------
 
