cdba3a9cab0bfdbb0f4544aa8111da72e1147b7a
Pranay <pranay2811@gmail.com>
2022-09-07 17:20:45 +0530

    Segregated functions, cleaned up code, added documentation

---
+++ R/datamodel.R
@@ -0,0 +1,91 @@
+#datamodel
+
+# Equipment Types --------------------------------------------------------------
+
+#' Equipment Types
+#' 
+#' Query all equipment types from Onboard's Data Model
+#' 
+#' @returns equip_type: A dataframe object
+#' @export
+get_equip_types <- function(){
+  
+  equiptype <-api.get('equiptype')
+  
+  subtypes <- sapply(equiptype$sub_types,as.data.frame)
+  subtypes <- data.table::rbindlist(subtypes)
+  
+  equip_types <- equiptype %>%
+    filter(active==T) %>%
+    select(-c(sub_types,critical_point_types,flow_order,active)) %>%
+    left_join(subtypes,by=c('id'='equipment_type_id'),
+              suffix = c('','_subtype'))
+  
+  return(equip_types)
+}
+
+
+
+# Point Types -------------------------------------------------------------
+
+#' Point Types
+#' 
+#' Queries all point types, measurements and their units from Onboard's Data Model and returns a clean output
+#' 
+#' @return point_types: A dataframe object
+#' @export
+get_point_types <- function(){
+  
+  pointtypes <- api.get('pointtypes')
+  
+  measurements <- api.get('measurements')
+  
+  # Get measurements and their associated units
+  units <- data.frame()
+  
+  for (i in 1:nrow(measurements)){
+    row_num =i
+    
+    measurement_single <- measurements[row_num,]
+    
+    measurement_units <- measurement_single$units
+    measurement_units <- data.table::rbindlist(measurement_units)
+    measurement_units[,'measurement_id']<- measurement_single$id
+    
+    units <- plyr::rbind.fill(units,measurement_units)
+  }
+  
+  units <- units %>%
+    select(measurement_id,
+           unit_name=name_long,
+           unit=name_abbr,data_type) %>%
+    mutate(measurement_id=as.integer(measurement_id))
+  
+  measurements_units <- left_join(measurements,
+                                  units,
+                                  by=c('id' = 'measurement_id')) %>%
+    select(id,
+           measurement_name=name,
+           units_convertible,
+           qudt_type,
+           unit_name,
+           unit,
+           data_type)
+  
+  #Unite data frames
+  point_types <- left_join(select(
+    pointtypes,id,tag_name,measurement_id,tags),
+    measurements_units,
+    by = c("measurement_id" = 'id')) %>%
+    mutate(across(tags,  ~ gsub('c\\(|\\)', '', .))) %>%
+    select(id,
+           point_type = tag_name,
+           measurement_name,
+           unit,
+           data_type,
+           tags)
+  
+  return(point_types)
+  
+}
+
---
+++ R/point_selector.R
@@ -0,0 +1,166 @@
+#Point Selector
+
+# Point Selector ----------------------------------------------------------
+
+#' PointSelector
+#' 
+#' A list of parameters to query metadata
+#' 
+#' @returns query: An empty list object
+#' 
+#' @examples 
+#' 
+#' query <- PointSelector()
+#' 
+#' query$buildings <- 101
+#' query$equipment_types <- 'ahu'
+#' query$point_types <- c('Supply Air Temperature','Supply Air Static Pressure')
+#' 
+#' @export
+PointSelector <- function(){
+  query <- list(orgs='',buildings='',point_ids='',point_names='',
+                point_topics='',updated_since='',point_types='',
+                equipment='',equipment_types='',point_hashes='')
+  
+  return(query)
+}
+
+
+
+# Select Points -----------------------------------------------------------
+
+#' Select Points
+#' 
+#' Returns a list of ids based on the input query from PointSelector. Uses http POST call to query data 
+#' 
+#' @param query query supplied from PointSelector
+#' 
+#' @examples 
+#' query <- PointSelector()
+#' 
+#' query$buildings <- 101
+#' query$equipment_types <- 'ahu'
+#' query$point_types <- c('Supply Air Temperature','Supply Air Static Pressure')
+#' 
+#' selection <- select_points(query)
+#' 
+#' @export
+select_points <- function(query){
+  
+  if(query$updated_since!='') {
+    updated_since <- as.numeric(
+      as.POSIXct(query$updated_since))
+  } else {
+    updated_since<- ''
+  }
+  
+  query <- query[names(query)!='updated_since']
+  
+  query_json <- query %>%
+    toJSON()
+  
+  query_json <- gsub('\\[""\\]','[]',query_json)
+  
+  if(updated_since!=''){
+    query_json <-
+      gsub('}',paste0(',"updated_since":',updated_since,'}'),query_json)
+  }
+  
+  endpoint <- 'points/select'
+  
+  point_selector_output <- api.post(endpoint,
+                                    json_body=query_json)
+  
+  return(point_selector_output)
+}
+
+
+# Points by ID ---------------------------------------------------------
+
+#' Points by ID
+#' 
+#' Queries data points by their ids
+#' 
+#' @param id (Integer) One or many point ids. 
+#' 
+#' @examples
+#' points <- get_points_by_ids(c(10000,10001))
+#' 
+#' If you are using the point selector function:
+#' query <- PointSelector()
+#' 
+#' query$buildings <- 101
+#' query$equipment_types <- 'ahu'
+#' query$point_types <- c('Supply Air Temperature','Supply Air Static Pressure')
+#' 
+#' selection <- select_points(query)
+#' 
+#' points <- get_points_by_ids(selection$points)
+#' 
+#' @export
+get_points_by_ids <- function(id){
+  
+  id_unlist <- unlist(id)
+  
+  #Separate point ids into chunks of 500
+  chunks <- split(id_unlist,
+                  ceiling(seq_along(id_unlist)/500))
+  
+  points<-data.frame()
+  
+  for(i in 1:length(chunks)){
+    
+    point_ids <- toJSON(chunks[[i]])
+    
+    #URL encode JSON list of points
+    point_ids <- URLencode(point_ids,reserved = T)
+    
+    endpoint <- paste0('points?point_ids=',point_ids)
+    
+    points_chunk <- api.get(endpoint)
+    
+    points <- plyr::rbind.fill(points,points_chunk)
+  }
+  
+  return(points)
+  
+}
+
+
+
+# Equipment by ID ---------------------------------------------------------
+
+#' Equipment by ID
+#' 
+#' Queries equipment by their ids
+#' 
+#' @param id (Integer) One or many equipment ids. 
+#' 
+#' @examples
+#' equipment <- get_equipment_by_ids(c(1000,1001))
+#' 
+#' If you are using the point selector function:
+#' query <- PointSelector()
+#' 
+#' query$buildings <- 101
+#' query$equipment_types <- 'ahu'
+#' 
+#' selection <- select_points(query)
+#' 
+#' equipment <- get_points_by_ids(selection$equipment)
+#' 
+#' @export
+get_equipment_by_ids <- function(id){
+  
+  id_unlist <- unlist(id)
+  
+  #Convert list of ids to JSON payload
+  id_json <- list(equipment_ids=id_unlist) %>%
+    toJSON()
+  
+  equipment <- api.post(endpoint='equipment/query',
+                        json_body = id_json)
+  
+  return(equipment)
+}
+
