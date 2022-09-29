19237f003430d24c947e85b7922c26ae5742b49a
Christopher <christopherdudasthomas@gmail.com>
2022-09-29 13:34:41 -0700

    removed _dependencies and added OnboardClient-package

---
+++ R/OnboardClient-package.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import dplyr
+#' @import httr
+#' @import rrapply
+#' @importFrom tidyr pivot_wider
+#' @importFrom jsonlite toJSON
+#' @importFrom jsonlite fromJSON
+#' @importFrom plyr rbind.fill
+#' @importFrom lubridate as_datetime
+#' @importFrom data.table rbindlist
+#' @importFrom rstudioapi askForSecret
+#' @importFrom jsonlite toJSON
+#' @importFrom jsonlite fromJSON
+#' @importFrom stringr str_split
+#' @importFrom tibble rownames_to_column
+## usethis namespace: end
+NULL
