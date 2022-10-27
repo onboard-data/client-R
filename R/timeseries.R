f209fc8e1b0302258aa5ae0afd37a8ff96c07a56
Pranay <pranay2811@gmail.com>
2022-10-27 16:17:43 -0400

    Added per-request unit conversion to timeseries functions. Added params and examples

--- R/timeseries.R
+++ R/timeseries.R
@@ -7,25 +7,59 @@
 #' @param start_time Start Time in UTC.
 #' @param end_time End Time in UTC.
 #' @param point_ids Point IDs for which timeseries data needs to be queried.
+#' @param units (Optional) A data.frame consisting of preferred units for given mesaurments
 #' 
 #' @return A long data.frame of time series data, with point id, timestamp, and raw point values as columns.
 #' 
+#' @examples
+#' \dontrun{
+#' 
+#' end_time <- as.POSIXlt(Sys.time(), tz = 'UTC')
+#' 
+#' start_time <- end_time - lubridate::hours(6)
+#' 
+#' point_ids <- c(290631, 290643) 
+#' 
+#' units <- data.frame("temperature" = "k")
+#' 
+#' timeseries <- get_timeseries_raw(start_time, end_time, point_ids, units)
+#' 
+#' }
+#' 
 #' @export
-get_timeseries_raw <- function(start_time, end_time, point_ids){
+get_timeseries_raw <- function(start_time, end_time, point_ids, units){
 
   start_time <- as.numeric(as.POSIXlt(start_time))
   
   end_time <- as.numeric(as.POSIXlt(end_time))
   
-  timeseries_query <- list(start = start_time,
-                           end = end_time,
-                           point_ids = point_ids) %>%
-    toJSON()
+
+    
+    timeseries_query <- list(start = start_time,
+                             end = end_time,
+                             point_ids = point_ids)
+    
+    if(!missing(units)){
+    
+    if(class(units) != "data.frame"){
+       stop("units must be a dataframe.")
+      
+    } else {
+      
+      timeseries_query <- append(timeseries_query,
+                                 list(units=units)) 
+    }
+  }
+  
+  
+  timeseries_query <- jsonlite::toJSON(timeseries_query)
   
   # Format JSON query
   timeseries_query <- gsub('start":\\[','start":', timeseries_query)
   timeseries_query <- gsub('\\],"end":\\[',',"end":', timeseries_query)
   timeseries_query <- gsub('\\],"point',',"point', timeseries_query)
+  timeseries_query <- gsub('units":\\[','units":', timeseries_query)
+  timeseries_query <- gsub('}\\]}','}}', timeseries_query)
   
   timeseries_output <- api.post(endpoint = 'timeseries',
                                 json_body = timeseries_query)
@@ -78,25 +112,42 @@ get_timeseries_raw <- function(start_time, end_time, point_ids){
 #' 
 #' @inheritParams get_timeseries_raw
 #' 
-#' @param unit Provide the unit type: "default" or "raw"
+#' @param unit_type Provide the unit type: "default" or "raw"
 #' 
 #' @return A wide data.frame of time-series data, with timestamp and all requested point IDs as columns.
 #' 
+#' @examples
+#' \dontrun{
+#' 
+#' end_time <- as.POSIXlt(Sys.time(), tz = 'UTC')
+#' 
+#' start_time <- end_time - lubridate::hours(6)
+#' 
+#' point_ids <- c(290631, 290643) 
+#' 
+#' units <- data.frame("temperature" = "k")
+#' 
+#' timeseries <- get_timeseries(start_time, end_time, point_ids, units)
+#' 
+#' }
+#' 
 #' @export
-get_timeseries <- function(start_time, end_time, point_ids,unit = 'default'){
+get_timeseries <- function(start_time, end_time, point_ids,units,
+                           unit_type = 'default'){
 
   timeseries_raw <- get_timeseries_raw(start_time = start_time,
                                        end_time = end_time,
-                                       point_ids = point_ids)
+                                       point_ids = point_ids,
+                                       units = units)
   if(nrow(timeseries_raw)!=0){
     
-    if(unit == 'default'){
+    if(unit_type == 'default'){
     timeseries_clean <- timeseries_raw %>% 
       transmute(.data$timestamp,
                 .data$point_id,
                 unit = as.character(.data$unit))
     
-    } else if (unit == 'raw'){
+    } else if (unit_type == 'raw'){
       timeseries_clean <- timeseries_raw %>% 
         transmute(.data$timestamp,
                   .data$point_id,
