24c745908709c3acac0731b44cb65516c2f2da1f
Pranay Shah <pranay2811@gmail.com>
2024-08-28 11:43:32 -0400

    Update columns to remove from staging data

--- R/staging.R
+++ R/staging.R
@@ -73,6 +73,7 @@ get_staged_data <- function(building, verbose = TRUE){
                    'timedelay','instance_tagger','ob_predicted',
                    'notif','acked','resolution','state_text',
                    'relinquish','priority','p\\.data\\.e\\.','_tagger',
+                   'p\\.confidences\\.e\\.',
                    'check','created','\\.err','tags',"type_id","unit_id",
                    #Device fields
                    "maxMaster","offset","objectList","systemStatus",
