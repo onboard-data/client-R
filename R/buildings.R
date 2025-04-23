79c89fc4f4a836c70118c9518be0813f24ca5163
Pranay Shah <pranay2811@gmail.com>
2025-04-23 15:17:05 -0400

    Fixed an issue with Point Selector

--- R/buildings.R
+++ R/buildings.R
@@ -120,8 +120,6 @@ select_points <- function(query, verbose = TRUE){
   # Wrap each non-empty field in a list
   query <- lapply(query, as.list)
   
-  query %>%  toJSON(pretty = TRUE,auto_unbox = TRUE)
-  
   # POST the query to the endpoint
   endpoint <- "points/select"
   response <- api.request(
